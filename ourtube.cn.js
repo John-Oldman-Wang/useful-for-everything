c2a56efb3e6cdefbc244aef73971d046e17c6be8
nokisnojok <mag1c_shen_09@hotmail.com>
2017-11-09 11:18:56 +0800

    add ourtube.js
    add ourtube.js

---
+++ ourtube.cn.js
@@ -0,0 +1,6 @@
+window.addEventListener('load',function(){
+    setTimeout(function() {
+        console.clear()
+        console.log('this was developed by wanglin(843022618@qq.com')
+    }, 3000);
+})
